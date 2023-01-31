347084823f7f44778a62a2744879d5a1a1df86ae
Chris Pham <Chris.Pham@veteransunited.com>
2023-01-31 16:31:17 -0600

    Solution doesn't pass for very large numbers on LeetCode server

--- FancySequence/Program.cs
+++ FancySequence/Program.cs
@@ -1,55 +1,117 @@
-﻿public class Fancy
+﻿Fancy fancy = new Fancy();
+fancy.Append(2);   // fancy sequence: [2]
+fancy.AddAll(3);   // fancy sequence: [2+3] -> [5]
+fancy.Append(7);   // fancy sequence: [5, 7]
+fancy.MultAll(2);  // fancy sequence: [5*2, 7*2] -> [10, 14]
+fancy.AddAll(3);   // fancy sequence: [10+3, 14+3] -> [13, 17]
+fancy.Append(10);  // fancy sequence: [13, 17, 10]
+fancy.MultAll(2);  // fancy sequence: [13*2, 17*2, 10*2] -> [26, 34, 20]
+Console.WriteLine(fancy.PrintSequence());
+Console.WriteLine("done");
+
+string[] inputInstructions = new string[] { "append", "append", "getIndex", "append", "getIndex", "addAll", "append", "getIndex", "getIndex", "append", "append", "getIndex", "append", "getIndex", "append", "getIndex", "append", "getIndex", "multAll", "addAll", "getIndex", "append", "addAll", "getIndex", "multAll", "getIndex", "multAll", "addAll", "addAll", "append", "multAll", "append", "append", "append", "multAll", "getIndex", "multAll", "multAll", "multAll", "getIndex", "addAll", "append", "multAll", "addAll", "addAll", "multAll", "addAll", "addAll", "append", "append", "getIndex" };
+int[] inputValues = new int[] { 12, 8, 1, 12, 0, 12, 8, 2, 2, 4, 13, 4, 12, 6, 11, 1, 10, 2, 3, 1, 6, 14, 5, 6, 12, 3, 12, 15, 6, 7, 8, 13, 15, 15, 10, 9, 12, 12, 9, 9, 9, 9, 4, 8, 11, 15, 9, 1, 4, 10, 9 };
+Fancy fancy2 = new Fancy();
+
+for (int i = 0; i < inputInstructions.Length; i++)
 {
+    switch (inputInstructions[i])
+    {
+        case "append":
+            fancy2.Append(inputValues[i]);
+            break;
+        case "addAll":
+            fancy2.AddAll(inputValues[i]);
+            break;
+        case "multAll":
+            fancy2.MultAll(inputValues[i]);
+            break;
+        case "getIndex":
+            fancy2.GetIndex(inputValues[i]);
+            break;
+    }
+}
+Console.WriteLine(fancy2.PrintSequence());
+
+public interface IFancy
+{
+    public void Append(int val);
+    public void AddAll(int inc);
+    public void MultAll(int m);
+    public long GetIndex(int idx);
+}
+
+public class Fancy: IFancy
+{
+    private List<int> _sequence;
+    private List<Instruction> _instructions;
+
     public Fancy()
     {
-        values = new List<int>();
-        instructions = new List<Instruction>();
+        _sequence = new List<int>();
+        _instructions = new List<Instruction>();
     }
 
-    public void Append(int val)
+    public string PrintSequence()
     {
-        values.Add(val);
+        string sequence = String.Empty;
+
+        for (int i = 0; i < _sequence.Count; i++)
+        {
+            long calculatedValue = GetIndex(i);
+            sequence += $"{calculatedValue} ";
+        }
+
+        return sequence;
     }
 
-    public void AddAll(int inc)
+    public void Append(int value)
     {
-        Instruction instruction = new Instruction(Operation.Add, inc);
-        instructions.Add(instruction);
+        _sequence.Add(value);
     }
 
-    public void MultAll(int m)
+    public void AddAll(int operand)
     {
-        Instruction instruction = new Instruction(Operation.Multiply, m);
-        instructions.Add(instruction);
+        Instruction instruction = new Instruction(Operation.Add, operand, _sequence.Count - 1);
+        _instructions.Add(instruction);
     }
 
-    public int GetIndex(int idx)
+    public void MultAll(int operand)
     {
-        if (idx > values.Count - 1)
+        Instruction instruction = new Instruction(Operation.Multiply, operand, _sequence.Count - 1);
+        _instructions.Add(instruction);
+    }
+
+    public long GetIndex(int index)
+    {
+        if (index > _sequence.Count - 1)
         {
             return -1;
         }
 
-        int product = values.ElementAt(idx);
+        long productAndSum = _sequence.ElementAt(index);
 
-        instructions.ForEach(i =>
+        for (int i = 0; i < _instructions.Count; i++)
         {
-            switch (i.operation)
+            Instruction instruction = _instructions[i];
+            if (!instruction.isApplicable(index))
+            {
+                continue;
+            }
+
+            switch (instruction.operation)
             {
                 case Operation.Add:
-                    product += i.value;
+                    productAndSum += instruction.operand;
                     break;
                 case Operation.Multiply:
-                    product *= i.value;
+                    productAndSum *= instruction.operand;
                     break;
             }
-        });
+        };
 
-        return product;
+        return productAndSum;
     }
-
-    private List<int> values;
-    private List<Instruction> instructions;
 }
 
 public enum Operation
@@ -60,12 +122,19 @@ public enum Operation
 
 public readonly struct Instruction
 {
-    public Instruction(Operation op, int val)
+    private readonly int _maxApplicableIndex;
+    public readonly Operation operation;
+    public readonly int operand;
+
+    public Instruction(Operation op, int val, int maxApplicableIndex)
     {
+        _maxApplicableIndex = maxApplicableIndex;
         operation = op;
-        value = val;
+        operand = val;
     }
 
-    public readonly Operation operation;
-    public readonly int value;
+    public bool isApplicable(int index)
+    {
+        return index <= _maxApplicableIndex;
+    }
 }
